bf44188acd5ec82c03f14e2a4288627c6dde5242
James Smith <james@floppy.org.uk>
2011-10-28 14:58:34 +0100

    initial framework generation. Rails 3.1.1 and RSpec 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoofprinter::Application.config.secret_token = '7da435c2d199c001cbacf968d11913eb182b954c5ef8b8050407dc5603b75943e7a5e8c5039db23a926f611846f041c0e9d4be9c11d5dfccaf64d2e89dce7b55'
