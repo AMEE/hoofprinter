3a0696c5b125ec0d5489e2ca4c1ad5d77024ebdc
James Smith <james@floppy.org.uk>
2011-10-28 18:21:57 +0100

    Add more calculation options

--- config/calculations/cows.rb
+++ config/calculations/cows.rb
@@ -4,3 +4,17 @@ calculation {
   path "/business/agriculture/livestock/entericfermentation"
   terms_from_amee
 }
+
+calculation {
+  label :manure
+  name "Cow Pats"
+  path "/business/agriculture/livestock/manure/ch4/cattleandswine"
+  terms_from_amee
+}
+
+calculation {
+  label :burgers
+  name "Cow Patties"
+  path "/embodied/clm/food"
+  terms_from_amee
+}
